bf01c83101ee61d1db57e79daf17f7a30f199e46
Max Saperstone <msaperst@gmail.com>
2025-02-28 16:44:22 -0700

    Trying better output passing/management

--- scraper/getSubmittedData.py
+++ scraper/getSubmittedData.py
@@ -50,6 +50,7 @@ def get_alumni_memories(gc):
 
     data = SubmittedData()
     for row in rows:
+        print(row)
         young_alumni = row[4] == 'Yes'
 
         if 'University' in row[3] and young_alumni:
